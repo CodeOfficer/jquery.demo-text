834a810bfff1774c193b1e640a6a4c0f0d593b1b
remi <remi@remitaylor.com>
2009-06-27 01:59:12 -0700

    updated so it clears demo-text from fields on form submission

--- jquery.demo-text.js
+++ jquery.demo-text.js
@@ -3,6 +3,16 @@ $(function(){
   $('*[data-demo-text]').each(function(){
     var element = $(this);
     var demo_text = element.attr('data-demo-text');
+    var form      = element.parents('form');
+
+    // if we submit the form, make sure to blank out these 
+    // fields (if they have the demo text in them)
+    form.submit(function(){
+      if (element.val() == demo_text) {
+        element.val('');
+      }
+    });
+
     if (demo_text != null) {
       if (element.val() == '' || element.val() == demo_text) {
         element.val( demo_text );
